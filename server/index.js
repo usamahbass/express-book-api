4fe91ab1ef17d426f1597134dceec5aca564af33
PondokProgrammer <afaizi1999@gmail.com>
2020-10-16 00:12:45 +0700

    delete body-parser and change to express parser, grouping code, add some comments,

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
@@ -19,36 +18,51 @@ let books = [
 app.use(cors());
 
 // body parser middleware
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get("/", (req, res) =>
   res.send(`
-  GET http://localhost:3000/semuabuku, untuk mendapatkan semua buku
-  POST http://localhost:3000/buku, untuk menambah buku
-  
+  http://localhost:3000/buku :
+    - method: GET, untuk mendapatkan semua buku
+    - method: POST, untuk menambah buku
+
+  http://localhost:3000/buku/:judul :
+    - method: GET, mendapatkan buku berdasarkan judul
+
+  http://localhost:3000/buku/:isbn :
+    - method: GET, mendapatkan buku berdasarkan isbn
+    - method: delete, menghapus buku
+    - method: PUT, mengganti buku yang ada dengan buku baru berdasarkan ISBN yang sama
 `)
 );
 
-app.post("/buku", (req, res) => {
-  const book = req.body;
+// Main route
+app.route('/buku')
+  // Get all books
+  .get((req, res) => {
+    res.json(books);
+  })
+  // Create new book
+  .post((req, res) => {
+    const book = req.body;
 
-  console.log(book);
-  books.push(book);
+    console.log(book);
+    books.push(book);
 
-  res.send({ response: "Buku telah ditambahkan !" });
-});
+    res.send({ response: "Buku telah ditambahkan !" });
+  })
 
-app.get("/semuabuku", (req, res) => {
-  res.json(books);
-});
-
-app.get("/buku/:judul", (req, res) => {
+// With "judul field"
+app.get("/buku/:judul", (req, res, next) => {
   // membaca judul dari url
 
   const judul = req.params.judul;
 
+  // Check if judul is exist
+  if (!judul) {
+    // throw to next middleware if not
+    return next()
+  }
   // cari buku dengan judul
 
   for (let book of books) {
@@ -63,39 +77,61 @@ app.get("/buku/:judul", (req, res) => {
   res.send({ response: "Buku tidak ditemukan !" });
 });
 
-app.delete("/buku/:isbn", (req, res) => {
-  // membaca isbn dari url
+// By ISBN
+app.route('/buku/:isbn')
+  // Get book by ISBN
+  .get((req, res) => {
+    // membaca judul dari url
 
-  const isbn = req.params.isbn;
+    const judul = req.params.judul;
 
-  // hapus buku dari array
+    // cari buku dengan judul
 
-  books = books.filter((buku) => {
-    if (buku.isbn !== isbn) {
-      return true;
+    for (let book of books) {
+      if (book.judul === judul) {
+        res.json(book);
+        return;
+      }
     }
-    return false;
-  });
 
-  res.send({ response: "Buku telah dihapus !" });
-});
+    // tampilkan 404 apabila isbn tidak ditemukan
 
-app.put("/buku/:isbn", (req, res) => {
-  // membaca isbn dari url
+    res.send({ response: "Buku tidak ditemukan !" });
+  })
+  // Delete book by ISBN
+  .delete((req, res) => {
+    // membaca isbn dari url
 
-  const isbn = req.params.isbn;
-  const newBook = req.body;
+    const isbn = req.params.isbn;
 
-  //  hapus buku dari array dan ganti dengan variabel newBook
+    // hapus buku dari array
 
-  for (let i = 0; i < books.length; i++) {
-    let book = books[i];
-    if (book.isbn === isbn) {
-      books[i] = newBook;
+    books = books.filter((buku) => {
+      if (buku.isbn !== isbn) {
+        return true;
+      }
+      return false;
+    });
+
+    res.send({ response: "Buku telah dihapus !" });
+  })
+  // Edit book by ISBN
+  .put((req, res) => {
+    // membaca isbn dari url
+
+    const isbn = req.params.isbn;
+    const newBook = req.body;
+
+    //  hapus buku dari array dan ganti dengan variabel newBook
+
+    for (let i = 0; i < books.length; i++) {
+      let book = books[i];
+      if (book.isbn === isbn) {
+        books[i] = newBook;
+      }
     }
-  }
 
-  res.send({ response: "Buku telah diubah !" });
-});
+    res.send({ response: "Buku telah diubah !" });
+  })
 
 app.listen(port, () => console.log(`Hello World from port ${port}`));
